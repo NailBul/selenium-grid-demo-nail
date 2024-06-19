535476d4f17fc3f9188b89d33fab39609213c71f
NailBul <nailbulbul@gmail.com>
2024-06-18 22:54:30 -0400

    Cloud-chrome added 2

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
                 },
         features = "src/test/resources/features",
         glue = "cydeo/steps",
+        tags = "@smoke",
         publish = true
 )
 public class CukesRunner {}
